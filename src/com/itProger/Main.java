f36099cbddb0d77362f658eefd2867059b4ebdf5
chas <chesnukaleksandr@gmail.com>
2023-09-03 03:51:37 +0300

    ADDing two java fIles

--- src/com/itProger/Main.java
+++ src/com/itProger/Main.java
@@ -2,7 +2,8 @@ package com.itProger;
 
 import java.sql.*;
 class Main {
-        public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        public static void main(String[] args)  {
+                System.out.println("Hello123");
 
                 }
         }
